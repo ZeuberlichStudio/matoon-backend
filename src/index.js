08cd23974b3e492036aee68ae4eec399bf31c380
Yaroslav <goooglemaaan@gmail.com>
2020-09-19 00:09:28 +0300

    dynamic port added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import express from 'express';
-//import cors from 'cors';
 import bodyParser from 'body-parser';
 import db from './database/connection';
 
 require('dotenv').config();
 const app = express();
-//app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/products', require('./routes/products'));
 app.use('/dev', require('./routes/dev'));
 
-app.listen(8080, () => console.log(`app is running on port ${8080}`));
+const server = app.listen(process.env.port || 3001, () => console.log(`app is running on port ${server.address().port}`));
