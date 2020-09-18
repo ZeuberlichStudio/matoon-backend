9d2dfe279c6403e255b51f81b27ebabb50607b50
Yaroslav <goooglemaaan@gmail.com>
2020-09-18 23:44:27 +0300

    changed server port

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import express from 'express';
-import cors from 'cors';
+//import cors from 'cors';
 import bodyParser from 'body-parser';
 import db from './database/connection';
 
 require('dotenv').config();
 const app = express();
-app.use(cors());
+//app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/products', require('./routes/products'));
 app.use('/dev', require('./routes/dev'));
 
-app.listen(3001, () => console.log(`app is running on port ${3001}`));
+app.listen(8080, () => console.log(`app is running on port ${8080}`));
