4a7b8a65013bf534a557f8b2c0383b2791b9838a
Yaroslav <goooglemaaan@gmail.com>
2020-10-28 21:54:47 +0300

    categories route added

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -2,6 +2,91 @@ const router = require('express').Router();
 
 import Cat from '../database/models/cat';
 
+router.get('/', (req, res) => {
+    const pipeline = [
+        {
+            $match: { parent: { $exists: true } }
+        },
+        {
+            $project: {
+                _id: 0,
+                posts: 0
+            }
+        },
+        {
+            $group: {
+                _id: '$parent',
+                children: { $push: '$$ROOT' }
+            }
+        },
+        {
+            $lookup: {
+                from: 'categories',
+                localField: '_id',
+                foreignField: 'slug',
+                as: '_id'
+            }
+        },
+        {
+            $unwind: {
+                path: '$_id'
+            }
+        },
+        {
+            $project: {
+                name: '$_id.name',
+                slug: '$_id.slug',
+                name: '$_id.name',
+                parent: '$_id.parent',
+                ancestors: '$_id.ancestors',
+                _id: 0,
+                children: '$children',
+
+            }
+        },
+        {
+            $group: {
+                _id: '$parent',
+                children: { $push: '$$ROOT' }
+            }
+        },
+        {
+            $match: { _id: { $ne: null } }
+        },
+        {
+            $lookup: {
+                from: 'categories',
+                localField: '_id',
+                foreignField: 'slug',
+                as: '_id'
+            }
+        },
+        {
+            $unwind: {
+                path: '$_id'
+            }
+        },
+        {
+            $project: {
+                name: '$_id.name',
+                slug: '$_id.slug',
+                name: '$_id.name',
+                parent: '$_id.parent',
+                ancestors: '$_id.ancestors',
+                _id: 0,
+                children: '$children',
+
+            }
+        }
+    ];
+
+    const getCatTree = Cat.aggregate(pipeline);
+
+    getCatTree
+        .then( data => res.send(data) )
+        .catch( err => res.send(err) );
+});
+
 router.get('/:slug', (req, res) => {
     const catSlug = req.params.slug;
 
