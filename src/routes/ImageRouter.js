dcd4b33ce48e6b8832ce308e929e7b21112f5099
Yaroslav Dimov <dymov@gmail.com>
2021-05-29 19:31:44 +0300

    image post/delete authorization;

--- src/routes/ImageRouter.js
+++ src/routes/ImageRouter.js
@@ -3,9 +3,10 @@ const ImageController = require('../controllers/image.controller');
 const uploadFiles = require('../middleware/fileUpload');
 const authorize = require('../middleware/authorize');
 
-router.use(authorize(['admin']));
 router.get('/:_id', ImageController.getImageRecord);
 router.get('/', ImageController.getImageRecordsList);
+
+router.use(authorize(['admin']));
 router.post('/', uploadFiles, ImageController.createImage);
 router.delete('/:_id', ImageController.deleteImage);
 
